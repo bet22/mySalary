c117a94fdc19b8291e59c8a7f19624c0d5f5d177
Alexey <34864818+bet22@users.noreply.github.com>
2021-07-30 10:35:41 +0700

    add dependency. Create main class

---
+++ src/main/java/ru/bet/Main.java
@@ -0,0 +1,11 @@
+package ru.bet;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+public class Main {
+    public static void main(String[] args) throws TelegramApiRequestException {
+        ApiContextInitializer.init();
+
+    }
+}
