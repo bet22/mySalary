65b076a75467f7bf256867cd15c0ea4dead1a0bd
Alexey <34864818+bet22@users.noreply.github.com>
2021-07-30 12:32:17 +0700

    add dependency for Value

--- src/main/java/ru/bet/TelegramBot.java
+++ src/main/java/ru/bet/TelegramBot.java
@@ -1,9 +1,14 @@
 package ru.bet;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${telegram.bot.token}")
+    private String token;
+
     @Override
     public void onUpdateReceived(Update update) {
 
